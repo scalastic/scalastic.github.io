c6f1268b66267a6c5978e3dc8411b02c5107c65c
jeanjerome <contact@scalastic.io>
2021-03-26 12:01:08 +0100

    Refactors Post Pagination plugin 🗿

--- _plugins/post-pagination.rb
+++ _plugins/post-pagination.rb
@@ -3,31 +3,26 @@ module Jekyll
 
     def initialize(tag_name, text, tokens)
       super
-      data = text.split('|')
-      @prev_url = data[0]
-      @prev_title = data[1]
-      @next_url = data[2]
-      @next_title = data[3]
+      @data = text.split('|')
     end
 
     def render(context)
 
     	tags = %(<div class="article-navigation">)	
-			tags += %(<a class="prev" href="#{@prev_url}">
+			tags += %(<a class="prev" href="#{@data[0]}">
 			  	<div class="article-nav article-nav-prev">
 			    	<i class="fas fa-angle-double-left"></i> Article Précédent
-			    	<h4 class="article-nav-title">#{@prev_title}</h4>
+			    	<h4 class="article-nav-title">#{@data[1]}</h4>
 			  	</div>
-			  </a>) if !@prev_url.nil? && !@prev_url.empty?
-			tags += %(<a class="next" href="#{@next_url}">
+			  </a>) if !@data[0].nil? && !@data[0].empty?
+			tags += %(<a class="next" href="#{@data[2]}">
 				  <div class="article-nav article-nav-next">
 				    Article Suivant <i class="fas fa-angle-double-right"></i>
-				    <h4 class="article-nav-title">#{@next_title}</h4>
+				    <h4 class="article-nav-title">#{@data[3]}</h4>
 				  </div>
-			  </a>) if !@next_url.nil? && !@next_url.empty?
+			  </a>) if !@data[2].nil? && !@data[2].empty?
 			tags += %(</div>)
-			tags
-		# "1:" + @prev_url + ":2:" +@prev_title + ":3:" +@next_url + ":4:" + @next_title
+
     end
 
   end
