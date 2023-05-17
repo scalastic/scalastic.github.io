c4440fef66d42f3d7031f4ca87911d4a24c14532
scalastic <contact@scalastic.io>
2023-05-18 01:11:05 +0200

    refacto: Change css shadow on code

--- _plugins/highlight-linedivs.rb
+++ _plugins/highlight-linedivs.rb
@@ -66,9 +66,9 @@ MSG
         <div class=\"window-controls\">
             <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"54\" height=\"14\" viewBox=\"0 0 54 14\">
                 <g fill=\"none\" fillRule=\"evenodd\" transform=\"translate(1 1)\">
-                    <circle cx=\"6\" cy=\"6\" r=\"6\" fill=\"#FF5F56\" stroke=\"#E0443E\" strokeWidth=\".5\" />
-                    <circle cx=\"26\" cy=\"6\" r=\"6\" fill=\"#FFBD2E\" stroke=\"#DEA123\" strokeWidth=\".5\" />
-                    <circle cx=\"46\" cy=\"6\" r=\"6\" fill=\"#27C93F\" stroke=\"#1AAB29\" strokeWidth=\".5\" />
+                    <circle cx=\"6\" cy=\"6\" r=\"6\" fill=\"#EC6A5E\" stroke=\"#EC6A5E\" strokeWidth=\".5\" />
+                    <circle cx=\"26\" cy=\"6\" r=\"6\" fill=\"#F4BF4F\" stroke=\"#F4BF4F\" strokeWidth=\".5\" />
+                    <circle cx=\"46\" cy=\"6\" r=\"6\" fill=\"#61C554\" stroke=\"#61C554\" strokeWidth=\".5\" />
                 </g>
             </svg>
         </div>
