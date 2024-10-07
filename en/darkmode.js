03677d3dfd7420d1510ca28932f40cfc28a7ffdd
jeanjerome <jeanjerome.levy@gmail.com>
2024-10-07 16:25:32 +0200

    Manual deployment

--- en/darkmode.js
+++ en/darkmode.js
@@ -1,3 +1,4 @@
+// Dark Mode
 const darkModeSwitches = document.querySelectorAll(".dark-mode-switch");
 
 if (darkModeSwitches) {
@@ -42,6 +43,7 @@ function initDarkMode() {
 
 initDarkMode();
 
+// Code Window
 // This assumes that you're using Rouge; if not, update the selector
 const codeBlocks = document.querySelectorAll('.highlight');
 const copyCodeButtons = document.querySelectorAll('.window-copy-icon');
@@ -67,3 +69,57 @@ copyCodeButtons.forEach((copyCodeButton, index) => {
     }, 1000);
   });
 });
+
+// Back to the Top with a Rocket
+function rocket(delay) {
+    document.addEventListener("DOMContentLoaded", function() {
+        const element = document.querySelector(".rocket");
+
+        if (element && element.offsetHeight === 0) {
+            element.classList.remove("show", "launch");
+            element.style.visibility = "hidden";
+        }
+
+        window.addEventListener("scroll", function() {
+            if (window.scrollY > 100) { // Si l'utilisateur a scrollé de plus de 100px
+                element.classList.add("show"); // Ajoute la classe "show"
+            } else {
+                element.classList.remove("show"); // Supprime la classe "show" si on est revenu en haut
+            }
+        });
+    });
+
+    // Action lorsque l'utilisateur clique sur le bouton "rocket"
+    const button = document.querySelector(".rocket");
+    if (button) {
+        button.addEventListener("click", function() {
+            // Ajoute la classe "launch" pour déclencher l'animation de lancement
+            button.classList.add("launch");
+            scrollToTop();
+
+            // Simule un autre élément avec un délai après le clic
+            const anotherElement = document.querySelector(".another-target");
+            if (anotherElement) {
+                anotherElement.style.transition = "visibility 0.3s ease";
+                anotherElement.style.visibility = "hidden"; // Masquer un autre élément après un délai
+                setTimeout(function() {
+                    anotherElement.style.visibility = "visible"; // Rendre visible après le délai
+                }, delay);
+            }
+
+            // Optionnel : enlever la classe "launch" après l'animation (ajustez selon vos besoins)
+            setTimeout(function() {
+                button.classList.remove("launch"); // Réinitialise l'état après le délai si nécessaire
+            }, 1500); // Par exemple, après 1.5 secondes
+        });
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // défilement en douceur
+    });
+}
+
+rocket(1000);
